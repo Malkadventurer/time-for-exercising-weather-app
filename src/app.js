8105063bfd49247dcce130e015225e6f6f9b1154
Malkadventurer <malkadventure@gmail.com>
2022-06-25 21:00:01 +0100

    added weather info in js

--- src/app.js
+++ src/app.js
@@ -22,3 +22,33 @@ let currentTime = document.querySelector("#clock");
 
 currentDate.innerHTML = `${day}`;
 currentTime.innerHTML = `${hours}:${minutes}`;
+
+function displayWeatherInformation(response) {
+  console.log(response);
+
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#dsc");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  humidityElement.innerHTML = `${Math.round(response.data.main.humidity)}%`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  feelsLikeElement.innerHTML = `${Math.round(response.data.main.feels_like)}°`;
+}
+
+let city = "Dublin, ie";
+let apiKey = "5df8aef5bdd4c12142826a987f87c062";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeatherInformation);
